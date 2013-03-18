b807c94ebff4b18e544b909d242cd56ba9fa4854
mjewkes <mjewkes@gmail.com>
2013-03-18 16:37:18 -0400

    wiki link to a stack

--- LearnMeACSharp/LearnMeACSharp/Program.cs
+++ LearnMeACSharp/LearnMeACSharp/Program.cs
@@ -7,6 +7,8 @@ namespace LearnMeACSharp
 {
     public class Program
     {
+
+        //Implement a Generic Stack http://en.wikipedia.org/wiki/Stack_%28abstract_data_type%29
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello Worlld");

e02d493728e0d5f307a9d215e52b2df56ed650a3
Brian <bdjewkes@gmail.com>
2013-03-18 19:37:54 -0400

    stack

--- LearnMeACSharp/LearnMeACSharp/Stack.cs
+++ LearnMeACSharp/LearnMeACSharp/Stack.cs
@@ -7,5 +7,20 @@ namespace LearnMeACSharp
 {
     public class Stack<T>
     {
+       List<T> list = new List<T>();
+
+       public void Push(T x){
+            list.Add(x);
+       }
+       public T Pop(){
+           T x = list[list.Count() - 1];
+           list.Remove(x);
+           return x;
+       }
+       public int Count
+       {
+           get { return list.Count(); }
+       }
+                    
     }
 }
